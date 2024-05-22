4e825ac59c9f38adc9e8b26602eb4eb34aca78a5
Claas Paaschburg <paaschburgc@gmail.com>
2024-05-22 15:22:13 +0200

    feat: display stat percentages

--- src/views/StatsView.vue
+++ src/views/StatsView.vue
@@ -3,24 +3,26 @@
 
   <h3>You</h3>
   <ul>
-    <li>Rock: {{ this.state.data.userRock }}</li>
-    <li>Paper: {{ this.state.data.userPaper }}</li>
-    <li>Scissors: {{ this.state.data.userScissors }}</li>
+    <li>Rock: {{ this.state.data.userRock }} - {{ this.percentageUserRock }}%</li>
+    <li>Paper: {{ this.state.data.userPaper }} - {{ this.percentageUserPaper }}%</li>
+    <li>Scissors: {{ this.state.data.userScissors }} - {{ this.percentageUserScissors }}%</li>
   </ul>
   <h3>Computer</h3>
   <ul>
-    <li>Rock: {{ this.state.data.computerRock }}</li>
-    <li>Paper: {{ this.state.data.computerPaper }}</li>
-    <li>Scissors: {{ this.state.data.computerScissors }}</li>
+    <li>Rock: {{ this.state.data.computerRock }} - {{ this.percentageComputerRock }}%</li>
+    <li>Paper: {{ this.state.data.computerPaper }} - {{ this.percentageComputerPaper }}%</li>
+    <li>
+      Scissors: {{ this.state.data.computerScissors }} - {{ this.percentageComputerScissors }}%
+    </li>
   </ul>
   <h3>Matches</h3>
   <ul>
-    <li>Matches won: {{ this.state.data.matchesWon }}</li>
-    <li>Matches lost: {{ this.state.data.matchesLost }}</li>
+    <li>Matches won: {{ this.state.data.matchesWon }} - {{ this.percentageMatchesWon }}%</li>
+    <li>Matches lost: {{ this.state.data.matchesLost }} - {{ this.percentageMatchesLost }}%</li>
     <br />
-    <li>Rounds won: {{ this.state.data.roundsWon }}</li>
-    <li>Rounds lost: {{ this.state.data.roundsLost }}</li>
-    <li>Rounds drawn: {{ this.state.data.drawn }}</li>
+    <li>Rounds won: {{ this.state.data.roundsWon }} - {{ this.percentageRoundsWon }}%</li>
+    <li>Rounds lost: {{ this.state.data.roundsLost }} - {{ this.percentageRoundsLost }}%</li>
+    <li>Rounds drawn: {{ this.state.data.drawn }} - {{ this.percentageRoundsDrawn }}%</li>
   </ul>
 
   <button @click="resetStats">Reset All Stats</button>
@@ -36,6 +38,49 @@ export default {
       state: statsStore()
     }
   },
+
+  computed: {
+    totalRounds() {
+      return this.state.data.userRock + this.state.data.userPaper + this.state.data.userScissors
+    },
+    percentageUserRock() {
+      return (this.state.data.userRock / this.totalRounds) * 100
+    },
+    percentageUserPaper() {
+      return (this.state.data.userPaper / this.totalRounds) * 100
+    },
+    percentageUserScissors() {
+      return (this.state.data.userScissors / this.totalRounds) * 100
+    },
+    percentageComputerRock() {
+      return (this.state.data.computerRock / this.totalRounds) * 100
+    },
+    percentageComputerPaper() {
+      return (this.state.data.computerPaper / this.totalRounds) * 100
+    },
+    percentageComputerScissors() {
+      return (this.state.data.computerScissors / this.totalRounds) * 100
+    },
+    percentageRoundsWon() {
+      return (this.state.data.roundsWon / this.totalRounds) * 100
+    },
+    percentageRoundsLost() {
+      return (this.state.data.roundsLost / this.totalRounds) * 100
+    },
+    percentageRoundsDrawn() {
+      return (this.state.data.drawn / this.totalRounds) * 100
+    },
+    totalMatches() {
+      return this.state.data.matchesWon + this.state.data.matchesLost
+    },
+    percentageMatchesWon() {
+      return (this.state.data.matchesWon / this.totalMatches) * 100
+    },
+    percentageMatchesLost() {
+      return (this.state.data.matchesLost / this.totalMatches) * 100
+    }
+  },
+
   methods: {
     resetStats() {
       localStorage.clear()
