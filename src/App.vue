4af42bcd763e5a4791cc7051e4d1847feb0da874
Claas Paaschburg <paaschburgc@gmail.com>
2024-05-23 16:38:44 +0200

    style: style header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <header class="site-header">
-    <h1><span>ROCK</span><span>PAPER</span><span>SCISSORS</span></h1>
+    <h1>
+      <span class="rock">ROCK</span>
+      <span class="paper">PAPER</span>
+      <span class="scissors">SCISSORS</span>
+    </h1>
   </header>
 
   <router-view />
@@ -9,8 +13,22 @@
 <style>
 .site-header {
   font-family: 'Permanent Marker', cursive;
+  font-size: 1.5rem;
   background-color: var(--main-color);
   width: 100vw;
   text-align: center;
+}
+.rock {
+  display: inline-block;
+  transform: rotate(-10deg);
+}
+.paper {
+  display: inline-block;
+  margin: 0 2rem;
+  transform: rotate(8deg);
+}
+.scissors {
+  display: inline-block;
+  transform: rotate(-5deg);
 }
 </style>
