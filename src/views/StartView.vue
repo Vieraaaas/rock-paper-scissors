4818f06b9d5c57a7803ab09657cf138d369664bb
Claas Paaschburg <paaschburgc@gmail.com>
2024-06-03 18:06:24 +0200

    style: enhance buttons in start view

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -9,3 +9,14 @@
 import { useI18n } from 'vue-i18n'
 const { t } = useI18n()
 </script>
+
+<style scoped>
+a {
+  display: block;
+  font-size: 2rem;
+  font-weight: bold;
+  text-align: center;
+  margin: 1rem;
+  padding: 1rem;
+}
+</style>
