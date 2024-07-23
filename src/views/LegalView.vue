b1b3627f23a470df4a01a6e472cb140b5d5f55eb
Claas Paaschburg <paaschburgc@gmail.com>
2024-07-23 18:09:22 +0200

    feat: add legal notice

--- src/views/LegalView.vue
+++ src/views/LegalView.vue
@@ -1,14 +1,48 @@
 <template>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis dolor aliquam excepturi
-    repellendus amet necessitatibus animi iste doloremque, distinctio ipsum inventore nesciunt
-    eaque. Numquam id atque, excepturi autem quos nesciunt?
-  </p>
+  <main>
+    <address>
+      <h2>{{ t('contact') }}</h2>
+      Claas Paaschburg <br />
+      Hauptstr. 11b <br />
+      23911 Einhaus <br />
+      E-Mail: <a href="mailto:claas@paaschburg.de">claas@paaschburg.de</a>
+    </address>
 
-  <router-link :to="{ name: 'Start' }">{{ t('back') }}</router-link>
+    <p>{{ t('disclaimer') }}</p>
+    <router-link class="back" :to="{ name: 'Start' }">{{ t('back') }}</router-link>
+  </main>
 </template>
 
 <script setup>
 import { useI18n } from 'vue-i18n'
 const { t } = useI18n()
 </script>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h2 {
+  margin: 1rem 0 0;
+}
+address {
+  font-style: normal;
+}
+
+address a {
+  font-style: italic;
+  font-weight: bold;
+  background-color: transparent;
+  outline: none;
+  margin: 0;
+  padding: 0;
+}
+
+p {
+  max-width: 60%;
+  font-style: italic;
+}
+</style>
